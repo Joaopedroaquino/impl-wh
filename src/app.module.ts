6a68a889a0173cb4a83280050bb06fe652f81fcb
joaopedroaquino <aqerhack@gmail.com>
2023-02-15 14:47:40 -0300

    Update app-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'; 
+import { HttpModule } from '@nestjs/axios'; 
+import { AppController } from './app.controller'; 
+import { AppService } from './app.service'; 
+ 
+ 
+@Module({ 
+  imports: [HttpModule],
+  controllers: [AppController], 
+  providers: [AppService], 
+}) 
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+export class AppModule {} 
