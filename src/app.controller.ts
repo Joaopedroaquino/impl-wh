421095072a434b3b0bfe478c0f7478fbda46d1ae
joaopedroaquino <aqerhack@gmail.com>
2023-02-15 14:46:28 -0300

    Create app-controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+import { Body, Controller, Post } from '@nestjs/common'; 
+import { AppService } from './app.service'; 
+ 
+ 
+@Controller() 
+export class AppController { 
+  constructor(private readonly appService: AppService) {} 
+ 
+ 
+  @Post('/order') 
+  createOrder(@Body() data) { 
+    const createdOrder = this.appService.createOrder(data); 
+ 
+ 
+    return createdOrder; 
+  } 
 }
