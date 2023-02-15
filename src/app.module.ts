bae84e192cb9adc3b522d7f87d0d96b4627af948
joaopedroaquino <aqerhack@gmail.com>
2023-02-15 14:58:57 -0300

    Add Schedulemodule for root in app-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios'; 
 import { AppController } from './app.controller'; 
 import { AppService } from './app.service'; 
+import { ScheduleModule } from '@nestjs/schedule';
  
  
 @Module({ 
-  imports: [HttpModule],
+  imports: [ScheduleModule.forRoot(), HttpModule ],
   controllers: [AppController], 
   providers: [AppService], 
 }) 
